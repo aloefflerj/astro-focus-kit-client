7400064885bf37307c79d0fd80a4ffa3c1c57a98
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-09-18 17:43:09 -0300

    feat: adds tasks page component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { TasksPage } from '../pages/TasksPage';
 
 export function Router() {
   return (
@@ -6,10 +7,11 @@ export function Router() {
       <Route path='/' element={<Navigate replace to="/tasks" />} />
       <Route path='/login' element={<h1>login</h1>} />
       <Route path='/register' element={<h1>register</h1>} />
-      <Route path='/tasks' element={<h1>tasks</h1>} />
+      <Route path='/tasks' element={<TasksPage />} />
       <Route path='/planet' element={<h1>planet</h1>} />
       <Route path='/journal' element={<h1>journal</h1>} />
       <Route path='/settings' element={<h1>settings</h1>} />
+      <Route path='/view' element={<h1>view</h1>} />
       <Route path='/note' element={<h1>note</h1>} />
       <Route path="*" element={<h1>not found</h1>} />
     </Routes>
