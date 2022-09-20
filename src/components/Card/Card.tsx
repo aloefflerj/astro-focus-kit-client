6abf527180b6df26c93f6397e7c6332a536285ac
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-09-19 22:41:07 -0300

    refactor: reestructures card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,16 +2,13 @@ import style from './Card.module.scss';
 
 interface Props {
   children: React.ReactNode;
-  type: 'optionsCard' | 'taskCard' | 'logoCard' | 'weekDayCard' | 'imageOptionCard';
-  active?: boolean
+  type: 'logo' | 'planetOption' | 'option' | 'dayHeader' | 'task';
 }
 
-export function Card({ children, type, active }: Props) {
+export function Card({ children, type }: Props) {
   return (
-    <div className={style.card}>
-      <span className={`${style.cardTop} ${style[type]} ${type} ${active === true ? 'active' : ''}`}>
-        {children}
-      </span>
+    <div className={`${style.card} ${style[type]}`}>
+      <span className={`${style.cardTop} ${style[type]}`}>{children}</span>
     </div>
   );
 }
