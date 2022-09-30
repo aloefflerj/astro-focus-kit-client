ba16f9ea71ff3a66f6a340d97473551f90a4a130
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-09-30 01:44:06 -0300

    feat: adds dashboard layout and restrict pages for login

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,23 +1,75 @@
 import { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ProtectedLayout } from '../components/ProtectedLayout/ProtectedLayout';
+import { DashboardLayoutPage } from '../pages/DashboardLayoutPage';
 import { LoginPage } from '../pages/LoginPage';
 import { TasksPage } from '../pages/TasksPage';
 
 export function Router() {
-  
-  return (
-      <Routes>
-        <Route path='/' element={<Navigate to='/tasks' />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<h1>register</h1>} />
-        <Route path='/tasks' element={<ProtectedLayout><TasksPage /></ProtectedLayout>} />
-        <Route path='/planet' element={<ProtectedLayout><h1>planet</h1></ProtectedLayout>} />
-        <Route path='/journal' element={<h1>journal</h1>} />
-        <Route path='/settings' element={<h1>settings</h1>} />
-        <Route path='/view' element={<h1>view</h1>} />
-        <Route path='/note' element={<h1>note</h1>} />
-        <Route path="*" element={<h1>not found</h1>} />
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<Navigate to='/tasks' />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<h1>register</h1>} />
+            <Route
+                path='/tasks'
+                element={
+                    <ProtectedLayout>
+                        <TasksPage />
+                    </ProtectedLayout>
+                }
+            />
+            <Route
+                path='/planet'
+                element={
+                    <ProtectedLayout>
+                        <DashboardLayoutPage>
+                            <h1>planet</h1>
+                        </DashboardLayoutPage>
+                    </ProtectedLayout>
+                }
+            />
+            <Route
+                path='/journal'
+                element={
+                    <ProtectedLayout>
+                        <DashboardLayoutPage>
+                            <h1>journal</h1>
+                        </DashboardLayoutPage>
+                    </ProtectedLayout>
+                }
+            />
+            <Route
+                path='/settings'
+                element={
+                    <ProtectedLayout>
+                        <DashboardLayoutPage>
+                            <h1>settings</h1>
+                        </DashboardLayoutPage>
+                    </ProtectedLayout>
+                }
+            />
+            <Route
+                path='/view'
+                element={
+                    <ProtectedLayout>
+                        <DashboardLayoutPage>
+                            <h1>view</h1>
+                        </DashboardLayoutPage>
+                    </ProtectedLayout>
+                }
+            />
+            <Route
+                path='/note'
+                element={
+                    <ProtectedLayout>
+                        <DashboardLayoutPage>
+                            <h1>note</h1>
+                        </DashboardLayoutPage>
+                    </ProtectedLayout>
+                }
+            />
+            <Route path='*' element={<h1>not found</h1>} />
+        </Routes>
+    );
 }
