555b6b3e142c56c25254dc79319cbaf02a250f96
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-21 01:10:02 -0300

    :chore: chore: removes old fake login api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,28 +2,9 @@ import axios from "axios";
 import { getUserLocalStorage } from "../contexts/AuthProvider/utils";
 import { EnvironmentConfig } from "../config/environmentConfig";
 
-export const loginApi = axios.create({
-    baseURL: "https://reqres.in/api/"
-});
-
 export const api = axios.create({
     baseURL: EnvironmentConfig.mainServerApiBasePath
 });
-
-loginApi.interceptors.request.use(
-    (config) => {
-        const user = getUserLocalStorage();
-
-        if (config.headers === undefined) return;
-
-        config.headers['x-access-token'] = user?.token;
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
 
 api.interceptors.request.use(
     (config) => {
