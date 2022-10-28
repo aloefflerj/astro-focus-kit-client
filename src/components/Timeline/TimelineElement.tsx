f145e0467056eab2be598326cbeb023e6fe8e6da
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-27 22:46:19 -0300

    :sparkles: feat: adds simple tasks history behaviour

--- src/components/Timeline/TimelineElement.tsx
+++ src/components/Timeline/TimelineElement.tsx
@@ -1,22 +1,35 @@
 import style from './TimelineElement.module.scss';
 import star from '../../assets/img/star.svg';
 import { MiniCard } from '../Card/MiniCard';
+import { useModalContext } from '../../hooks/useModalContext';
+import { Modal } from '../Modal/Modal';
 
 export function TimelineElement({
     children,
+    setShowDetails,
 }: {
     children: JSX.Element | JSX.Element[] | string;
+    setShowDetails: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+    const { openModal } = useModalContext();
     return (
-        <li className={style.timelineElement}>
-            <div className={style.starImgWrapper}>
-                <img src={star} className={style.starImg} />
-            </div>
-            <div className={style.cardElement}>
-                <div className='cardStyle'>
-                    <span className='cardStyleTop'>{children}</span>
+        <>
+            <li className={style.timelineElement}>
+                <div className={style.starImgWrapper}>
+                    <img src={star} className={style.starImg} />
                 </div>
-            </div>
-        </li>
+                <div className={style.cardElement}>
+                    <div
+                        className='cardStyleHover'
+                        onMouseEnter={() => setShowDetails(true)}
+                        onMouseLeave={() => setShowDetails(false)}
+                        onClick={e => openModal(e, 'history-details')}
+                    >
+                        <span className='cardStyleTop'>{children}</span>
+                    </div>
+                </div>
+            </li>
+            <Modal modalId='history-details'>Task History Modal</Modal>
+        </>
     );
 }
--- src/pages/JournalPage.tsx
+++ src/pages/JournalPage.tsx
@@ -1,26 +1,34 @@
+import { useState } from 'react';
 import { Card } from '../components/Card/Card';
 import { Timeline } from '../components/Timeline/Timeline';
 import { TimelineElement } from '../components/Timeline/TimelineElement';
 import { DashboardLayoutPage } from './DashboardLayoutPage';
 
 export function JournalPage() {
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
         <DashboardLayoutPage>
             <h1>Your Tasks by Week</h1>
             <Timeline>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
-                <TimelineElement>OI</TimelineElement>
+                <TimelineElement 
+                    setShowDetails={setShowDetails}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'flex-start',
+                        }}
+                    >
+                        <span>10 Jan » 17 Jan</span>
+                        {showDetails && (
+                            <div>
+                                <span style={{ paddingLeft: '12px' }}>8 Finished Tasks</span>
+                                <span style={{ paddingLeft: '12px' }}>2 Unfinished Tasks</span>
+                            </div>
+                        )}
+                    </div>
+                </TimelineElement>
             </Timeline>
         </DashboardLayoutPage>
     );
