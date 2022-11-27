b78ab3392fdcb1f2d74b491d1f369d9078649054
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-27 11:51:37 -0300

    :sparkles: feat: adds styling to settings options

--- src/elements/Settings/SettingsMain.tsx
+++ src/elements/Settings/SettingsMain.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Option } from '../Sidebar/Option';
 import { Settings } from './Settings';
+import style from './SettingsMain.module.scss';
 
 export function SettingsMain() {
     const basePath = '/settings';
@@ -9,12 +10,16 @@ export function SettingsMain() {
         <>
             <h1>Settings</h1>
             <Settings>
-                <Link to={`${basePath}/blocked-sites`}>
-                    <Option title='Websites Block' />
-                </Link>
-                <Link to={`${basePath}/timer`}>
-                    <Option title='Procrastination Timer' />
-                </Link>
+                <div className={style.settingsMain}>
+                    <div className={style.settingsMainContent}>
+                        <Link to={`${basePath}/blocked-sites`}>
+                            <Option title='Websites Block' />
+                        </Link>
+                        <Link to={`${basePath}/timer`}>
+                            <Option title='Procrastination Timer' />
+                        </Link>
+                    </div>
+                </div>
             </Settings>
         </>
     );
